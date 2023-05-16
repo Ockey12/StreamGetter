750ac79454a106d179286ccb54642180920a2d73
OnagaHaruki <haloupgarou@gmail.com>
2023-05-16 19:15:16 +0900

    Initial Commit

---
+++ StreamGetter/StreamGetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StreamGetterApp.swift
+//  StreamGetter
+//
+//  Created by オナガ・ハルキ on 2023/05/16.
+//
+
+import SwiftUI
+
+@main
+struct StreamGetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
