eb702647c16f56303f5d2350dfa3342f8bb2de08
OnagaHaruki <haloupgarou@gmail.com>
2023-05-27 23:26:24 +0900

    clean: コメントアウトしていたコードを削除した

--- StreamGetter/Model/ChannelRequester.swift
+++ StreamGetter/Model/ChannelRequester.swift
@@ -8,53 +8,6 @@
 import Foundation
 
 struct ChannelRequester {
-    //    func searchChannels(channelKeyWord: String, completion: @escaping (Result<[Channel], Error>) -> Void) {
-    //        var components = URLComponents()
-    //        components.scheme = "https"
-    //        components.host = "www.googleapis.com"
-    //        components.path = "/youtube/v3/search"
-    //
-    //        let apiKey = ApiKeyGetter.YouTubeApiKey
-    //        components.queryItems = [
-    //            URLQueryItem(name: "part", value: "snippet"),
-    //            URLQueryItem(name: "q", value: channelKeyWord),
-    //            URLQueryItem(name: "type", value: "channel"),
-    //            URLQueryItem(name: "key", value: apiKey),
-    //            URLQueryItem(name: "maxResults", value: "20")
-    //        ]
-    //
-    //        guard let url = components.url else {
-    //            completion(.failure(ApiError.invalidURL))
-    //            return
-    //        }
-    //
-    //        URLSession.shared.dataTask(with: url) { (data, response, error) in
-    //            if let error = error {
-    //                completion(.failure(error))
-    //                return
-    //            }
-    //
-    //            guard let data = data else {
-    //                completion(.failure(ApiError.noData))
-    //                return
-    //            }
-    //
-    //            if let json = String(data: data, encoding: .utf8) {
-    //                print("Response JSON: \(json)")
-    //            }
-    //
-    //            do {
-    //                let decoder = JSONDecoder()
-    //                let response = try decoder.decode(ChannelResponse.self, from: data)
-    //                let channels = response.items.map(Channel.init)
-    //                completion(.success(channels))
-    //            } catch {
-    //                print("Decoding error: \(error)")
-    //                completion(.failure(error))
-    //            }
-    //        }.resume()
-    //    } // end func searchChannels
-    
     func searchChannels(channelKeyWord: String) async throws -> [Channel] {
         var components = URLComponents()
         components.scheme = "https"
