949317a996b0a8c5ad4d7083f19db48962b3d45f
OnagaHaruki <haloupgarou@gmail.com>
2023-05-28 21:24:43 +0900

    add: SearchChannelsView

---
+++ StreamGetter/Test/SearchChannelsView.swift
@@ -0,0 +1,20 @@
+//
+//  SearchChannelsView.swift
+//  StreamGetter
+//
+//  Created by オナガ・ハルキ on 2023/05/28.
+//
+
+import SwiftUI
+
+struct SearchChannelsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SearchChannelsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchChannelsView()
+    }
+}
